926ca2d12632408c332770b1d6bf902080926e6e
esther <esther@2064896474474.altostrat.com>
2025-07-02 17:21:53 +0000

    fix: folders

--- adk-python-quickstart/idx-template.nix
+++ adk-python-quickstart/idx-template.nix
@@ -3,9 +3,9 @@
     pkgs.python3
   ];
   bootstrap = ''
-    mkdir -p "$out"
-    cp -rf ${./.} "$out"
-    chmod -R u+w "$out"
-    rm -rf "$out/.git" "$out/idx-template".{nix,json}
+    cp -rf ${./.}/multi_tool_agent "$WS_NAME"
+    chmod -R +w "$WS_NAME"
+    chmod +x "$WS_NAME"/devserver.sh
+    mv "$WS_NAME" "$out"
   '';
 }
